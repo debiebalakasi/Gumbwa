d22c25b2b12b29da6a53936566a121bc4707ac1b
Deborah Balakasi <debiebalakasi@gmail.com>
2026-05-29 14:05:00 +0200

    Update ProductController.php

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -13,9 +13,17 @@ class ProductController extends Controller
         $products = Product::latest()->get();
         return view('admin.products', compact('products'));
     }
-
-    public function store(Request $request)
+public function store(Request $request)
 {
-    dd($request->all());
+    Product::create([
+        'name' => $request->name,
+        'description' => $request->description,
+        'price' => $request->price,
+        'image' => $request->image,
+        'available' => $request->has('available')
+    ]);
+
+    return redirect('/admin/products')
+        ->with('success', 'Product added successfully');
 }
 }
