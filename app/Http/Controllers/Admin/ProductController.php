3c313b431646b55256ba6e8579466e4b2746a9b5
Deborah Balakasi <debiebalakasi@gmail.com>
2026-05-29 13:45:00 +0200

    product controller update

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -15,16 +15,7 @@ class ProductController extends Controller
     }
 
     public function store(Request $request)
-    {
-        Product::create([
-            'name' => $request->name,
-            'description' => $request->description,
-            'price' => $request->price,
-            'image' => $request->image,
-            'available' => $request->has('available')
-        ]);
-
-        return redirect('/admin/products')
-            ->with('success', 'Product added successfully');
-    }
+{
+    dd($request->all());
+}
 }
--- resources/views/admin/products.blade.php
+++ resources/views/admin/products.blade.php
@@ -26,7 +26,7 @@
         Available
     </label>
 
-    <button class="bg-green-600 text-white px-4 py-2 rounded">
+    <button type="submit" class="bg-green-600 text-white px-4 py-2 rounded">
         Add Product
     </button>
 </form>
