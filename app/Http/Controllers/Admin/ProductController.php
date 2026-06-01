1d9d1166c3bf5011868f90cb4f37abebf7e4316e
Deborah Balakasi <debiebalakasi@gmail.com>
2026-06-01 16:01:38 +0200

    Update ProductController.php

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -15,7 +15,7 @@ class ProductController extends Controller
     }
 public function store(Request $request)
 {
-      dd('Store method reached');
+      dd($request->all());
     Product::create([
         'name' => $request->name,
         'description' => $request->description,
